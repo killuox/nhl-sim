4f27a13285033b63633109f3d4799c3f1e2b53fd
Antonin Arsenault <aarsenault@korsr.studio>
2024-06-25 22:20:17 -0400

    Improve some code

--- sim/main.go
+++ sim/main.go
@@ -36,7 +36,11 @@ func main() {
 
 	// Validate if the teams provided exist in the data
 	if team1Data != nil && team2Data != nil {
-		startGame(team1Data, team2Data)
+		game := Game{
+			Team1: *team1Data,
+			Team2: *team2Data,
+		}
+		game.start()
 	} else {
 		fmt.Println("Please provide a valid NHL Team")
 	}
@@ -74,30 +78,20 @@ func getRandomGoal(goalsOdds map[float64]float64, off float64, def float64) floa
 	return 0 // Fallback in case something goes wrong
 }
 
-func decideTieBreaker(team1, team2 data.Team) (float64, float64) {
-	team1Chances := team1.Rank.Ovr - team2.Rank.Ovr
-	team2Chances := team2.Rank.Ovr - team1.Rank.Ovr
-
-	// Decide the winner with a random number
-	team1Random := rand.Float64() * team1Chances
-	team2Random := rand.Float64() * team2Chances
-
-	if team1Random > team2Random {
-		return 1, 0
-	} else {
-		return 0, 1
-
-	}
+type Game struct {
+	Team1 data.Team
+	Team2 data.Team
 }
 
-func startGame(team1, team2 *data.Team) {
+func (g *Game) start() {
 	fmt.Println("The game is starting!")
 	isOvertime := false
-	team1Score := getRandomGoal(data.GoalsOdds, team1.Rank.Off, team2.Rank.Def)
-	team2Score := getRandomGoal(data.GoalsOdds, team2.Rank.Off, team1.Rank.Def)
+	// Calculate goals based on the teams' ranks off vs def
+	team1Score := getRandomGoal(data.GoalsOdds, g.Team1.Rank.Off, g.Team2.Rank.Def)
+	team2Score := getRandomGoal(data.GoalsOdds, g.Team2.Rank.Off, g.Team1.Rank.Def)
 
 	if team1Score == team2Score {
-		team1Result, team2Result := decideTieBreaker(*team1, *team2)
+		team1Result, team2Result := g.overtime(g.Team1, g.Team2)
 
 		team1Score += team1Result
 		team2Score += team2Result
@@ -106,10 +100,27 @@ func startGame(team1, team2 *data.Team) {
 	}
 
 	fmt.Println("---------------")
-	fmt.Printf("%s: %v\n", team1.Name, team1Score)
-	fmt.Printf("%s: %v\n", team2.Name, team2Score)
+	fmt.Printf("%s: %v\n", g.Team1.Name, team1Score)
+	fmt.Printf("%s: %v\n", g.Team2.Name, team2Score)
 	if isOvertime {
 		fmt.Println("OT")
 	}
 	fmt.Println("---------------")
+}
+
+func (g *Game) overtime(team1 data.Team, team2 data.Team) (float64, float64) {
+	team1Chances := team1.Rank.Ovr - team2.Rank.Ovr
+	team2Chances := team2.Rank.Ovr - team1.Rank.Ovr
+
+	rangeAdjustment := 100.0
+
+	// Generate random numbers within adjusted ranges
+	team1Random := rand.Float64() * (1 + float64(team1Chances)/rangeAdjustment)
+	team2Random := rand.Float64() * (1 + float64(team2Chances)/rangeAdjustment)
+
+	if team1Random > team2Random {
+		return 1, 0 // Team1 wins
+	} else {
+		return 0, 1 // Team2 wins
+	}
 }
